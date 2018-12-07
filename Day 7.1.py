59897c04a61f9904db47aed91bc5f15eca939fab
Schuster <rschuster@eab.com>
2018-12-07 17:04:48 -0500

    Adds Day 7, Part 1 solution

---
+++ Day 7.1.py
@@ -0,0 +1,63 @@
+with open('Inputs\Day 7.txt','r') as f:
+    inputList = [_ for _ in f]
+
+lines = [x.split() for x in inputList]
+rules = [[line[1],line[7]] for line in lines]
+# ['C', 'A'] = C must be finished before A can start
+
+before = [_[0] for _ in rules]
+after = [_[1] for _ in rules]
+   
+##print('Before:',before)
+##print('After:',after)
+
+ordering = {}
+for i,letter in enumerate(after):
+    if letter not in ordering.keys():
+        ordering[letter] = [before[i]]
+    else:
+        ordering[letter].append(before[i])
+    ordering[letter].sort()
+#'E': ['B', 'D', 'F'] = B, D, and F must be finished before E can start
+
+start = [x for x in set(before) if x not in set(after)]
+
+answer = [min(start)]
+workingLetters = []
+
+for i in start:
+    if i == min(start):
+        continue
+    workingLetters.append(i)
+
+i = 0
+while len(answer) < len(set(before)|set(after)):
+    curLetter = answer[i]
+    #go thru every key and see if the current letter is in the value list
+    #delete it if true
+    #if the length of the key's new value is 0, put the key into workingLetters and delete it
+    for key in ordering.keys():
+        if curLetter in ordering[key]:
+            ordering[key].remove(curLetter)
+            if len(ordering[key]) == 0:
+                workingLetters.append(key)
+    for j in workingLetters:
+        if j in ordering.keys():
+            ordering.pop(j, None)
+    if len(workingLetters) > 0:
+        answer.append(min(workingLetters))
+        i += 1
+        workingLetters.remove(answer[i])
+
+print(''.join(answer))
+
+
+
+
+
+
+
+
+
+
+        
