829908844896624776e02473a5cb61fcb69b3998
Schuster <raphi.schuster@outlook.com>
2018-12-18 13:46:13 -0500

    Adds Day 8.1 solution

--- Day 8.1.py
+++ Day 8.1.py
@@ -1,35 +1,47 @@
+from collections import deque
+
 with open('Inputs/Day 8.txt','r') as f:
     inputList = list(map(int,[_.split(' ') for _ in f.readlines()][0]))
 #   
-inputList = [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2]
+##inputList = [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2]
 #   
 
-answer = []
+metaSum = 0
+metaQ = deque([])
+childQ = deque([])
 
-##def builder():
-##    global inputList
-##    global answer
-##    children = inputList.pop(0)
-##    metadata = inputList.pop(0)
-##    for i in range(children + metadata):
-##        answer.append([])
-##    return first, second
-##    
-##while len(inputList) > 0:
-##    first, second = builder()
-##    if first > 0:
-##        continue
+# Start by pulling first header
+childQ.append(inputList.pop(0))
+metaQ.append(inputList.pop(0))
 
-childC = 0
-metaC = 0
-
-while len(inputList) > 0:
-    first = inputList.pop(0)
-    second = inputList.pop(0)
-    if first == 0:
-        print('yay')
-    childC += first
-    metaC += second
+while True:
+    # if the current child has children
+    # then grab the header data
+    if childQ[-1] != 0:
+        childQ.append(inputList.pop(0))
+        metaQ.append(inputList.pop(0))
+            
+    # if the current child has no children
+    # then grab the number of metadata points based on last value in metaQ
+    # and remove the last values from the queue (LIFO)
+    # and, if there is a parent, reduce the expected number of children on the parent
+    elif childQ[-1] == 0:
+        for i in range(metaQ[-1]):
+            metaSum += inputList.pop(0)
+        metaQ.pop()   
+        childQ.pop()
+        if len(childQ) > 0:
+            childQ[-1] -= 1
+    # if current child has negative children
+    # then exit loop
+    else:
+        print('You cannot have negative children')
+        break
+            
+    # if the number of points you are looking for is the same as the number of points remaining
+    # then add all the points and exit loop
+    if len(inputList) == sum(childQ):
+        metaSum += sum(childQ)
+        break
     
-
-print(answer)
+print(metaSum)
